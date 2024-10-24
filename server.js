b459ead0975b75f92579c82d08a3ce11c67ba45d
YidanWWW <wang.yidan2@northeastern.edu>
2024-10-24 16:41:07 -0400

    add

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const app = require("./app");
-const { initializeDatabase } = require("./services/databaseService");
+const sequelize = require("./config/db");
 
-initializeDatabase()
+sequelize
+  .sync({ alter: true })
   .then(() => {
+    console.log("Database synchronized");
     const PORT = process.env.PORT || 8080;
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => {
-    console.error("Error during database initialization:", err);
+    console.error("Error synchronizing the database:", err);
   });
