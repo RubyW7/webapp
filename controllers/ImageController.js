a731e7c88bac5191685f6b06b8fd5abf2985ffe7
YidanWWW <wang.yidan2@northeastern.edu>
2024-10-30 22:59:08 -0400

    add logger and stats

--- controllers/ImageController.js
+++ controllers/ImageController.js
@@ -3,39 +3,41 @@ const s3Config = require("../config/s3Config");
 const { v4: uuidv4 } = require('uuid');
 const moment = require('moment');
 const User = require('../models/user');
+const logger = require('../utils/logger');
+const statsDClient = require('../utils/metrics');
 
 const generateFileUrl = (userId, filename) => {
     return `${s3Config.aws.bucket}/${userId}/${filename}`;
 };
 
 exports.uploadProfilePic = async (req, res) => {
+    statsDClient.increment('endpoint.uploadProfilePic.hit');
+    logger.info('Entering uploadProfilePic method');
+    
     try {
         if (req.files && req.files.profilePic) {
             const userId = req.user.id;
             const user = await User.findByPk(userId);
+            logger.info(`Current profile_image for user ${userId}: ${user.profile_image}`);
 
-            // check if profile image already existed
-            console.log("Current profile_image:", user.profile_image);
             if (user.profile_image) {
-                console.log("User already has a profile picture.");
+                logger.warn(`User ${userId} already has a profile picture.`);
                 return res.status(400).json({
                     message: "Profile picture already exists. Please delete it before uploading a new one."
                 });
             }
 
-            // upload to S3
             const result = await uploadToS3(req.files.profilePic, s3Config.aws.bucket);
-            if (!result ) {
-                console.log("Failed to upload image to S3.");
+            if (!result) {
+                logger.error(`Failed to upload image to S3 for user ${userId}.`);
+                statsDClient.increment('endpoint.uploadProfilePic.fail.s3Upload');
                 return res.status(500).json({
                     message: "Failed to upload image to S3."
                 });
             }
-            const upload_date = moment().format("YYYY-MM-DD");
 
-            // upldate user.profile_image
-            user.profile_image = result.filename;;
-            user.upload_date = upload_date;
+            user.profile_image = result.filename;
+            user.upload_date = moment().format("YYYY-MM-DD");
             await user.save(); 
 
             const url = generateFileUrl(userId, user.profile_image);
@@ -44,23 +46,25 @@ exports.uploadProfilePic = async (req, res) => {
                 file_name: req.files.profilePic.name,
                 id: uuidv4(),
                 url: url,
-                upload_date: upload_date,
+                upload_date: user.upload_date,
                 user_id: userId,
             };
 
+            statsDClient.increment('endpoint.uploadProfilePic.success');
             return res.status(201).json({
                 message: "Success",
                 body: responseBody
             });
         } else {
-            console.log("No profile picture file found in the request.");
+            logger.warn("No profile picture file found in the request.");
             return res.status(400).json({
                 message: "Bad Request",
                 error: "No profile picture file found in the request"
             });
         }
     } catch (e) {
-        console.log("Error in uploadProfilePic:", e);
+        logger.error(`Error in uploadProfilePic: ${e}`);
+        statsDClient.increment('endpoint.uploadProfilePic.error');
         return res.status(500).json({
             message: "Something went wrong!"
         });
@@ -68,49 +72,76 @@ exports.uploadProfilePic = async (req, res) => {
 };
 
 exports.getProfilePic = async (req, res) => {
+    // Increment the hit counter for this endpoint
+    statsDClient.increment('endpoint.getProfilePic.hit');
+    logger.info('Entering getProfilePic method');
+
     try {
         const userId = req.user.id;
+        logger.debug(`Fetching profile picture for user ID: ${userId}`);
+
         const user = await User.findByPk(userId);
 
         if (!user.profile_image) {
+            logger.warn(`Profile picture not found for user ID: ${userId}`);
+            statsDClient.increment('endpoint.getProfilePic.notFound');
             return res.status(404).json({ message: "Profile picture not found." });
         }
 
         const url = generateFileUrl(userId, user.profile_image);
+        const uploadDateFormatted = moment(user.upload_date).format("YYYY-MM-DD");
 
         const responseBody = {
             file_name: user.profile_image,
             id: user.id,
             url: url,
-            upload_date: moment(user.upload_date).format("YYYY-MM-DD"),
+            upload_date: uploadDateFormatted,
             user_id: user.id,
         };
 
+        // Successful retrieval of profile picture
+        statsDClient.increment('endpoint.getProfilePic.success');
+        logger.info(`Profile picture retrieved successfully for user ID: ${userId}`);
         return res.status(200).json(responseBody);
     } catch (error) {
-        console.error("Error in getProfilePic:", error);
+        logger.error(`Error in getProfilePic for user ID: ${req.user.id}: ${error}`);
+        statsDClient.increment('endpoint.getProfilePic.failure');
         return res.status(500).json({ message: "Something went wrong!" });
     }
 };
 
 exports.deleteProfilePic = async (req, res) => {
+    // Increment the hit counter for this endpoint
+    statsDClient.increment('endpoint.deleteProfilePic.hit');
+    logger.info('Entering deleteProfilePic method');
+
     try {
         const userId = req.user.id;
+        logger.debug(`Attempting to delete profile picture for user ID: ${userId}`);
+
         const user = await User.findByPk(userId);
 
         if (!user.profile_image) {
+            logger.warn(`Profile picture not found for user ID: ${userId}`);
+            statsDClient.increment('endpoint.deleteProfilePic.notFound');
             return res.status(404).json({ message: "Profile picture not found." });
         }
 
         const filename = user.profile_image;
-        await deleteFromS3(filename, s3Config.aws.bucket); 
+        await deleteFromS3(filename, s3Config.aws.bucket);
+        logger.info(`Profile picture ${filename} deleted from S3 for user ID: ${userId}`);
+
         user.profile_image = null;
         user.upload_date = null; 
         await user.save();
 
+        // Successfully deleted the profile picture
+        statsDClient.increment('endpoint.deleteProfilePic.success');
+        logger.info(`Profile picture data removed from database for user ID: ${userId}`);
         return res.status(204).send();
     } catch (error) {
-        console.error("Error in deleteProfilePic:", error);
+        logger.error(`Error in deleteProfilePic for user ID: ${req.user.id}: ${error}`);
+        statsDClient.increment('endpoint.deleteProfilePic.failure');
         return res.status(500).json({ message: "Something went wrong!" });
     }
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,22 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const logger = require('../utils/logger');
+const statsDClient = require('../utils/metrics');
 
 exports.getUser = async (req, res) => {
+  logger.info('GET: ENTERING getUser controller method');
+  statsDClient.increment('endpoints.request.http.get.getUser');
   try {
     if (Object.keys(req.query).length > 0) {
-      return res.status(400).send();
+      logger.warn('GET: Invalid query parameters in getUser');
+      statsDClient.increment('endpoints.response.http.get.fail.getUser');
+      return res.status(400).send({ message: "Query parameters are not allowed" });
     }
 
     const user = req.user;
-
     res.header("Accept", "application/json");
-
+    statsDClient.increment('endpoints.response.http.get.success.getUser');
     return res.status(200).json({
       id: user.id,
       first_name: user.first_name,
@@ -21,28 +26,34 @@ exports.getUser = async (req, res) => {
       account_updated: user.account_updated,
     });
   } catch (error) {
+    logger.error(`GET: getUser error - ${error}`);
+    statsDClient.increment('endpoints.response.http.get.failure.getUser');
     console.error("Error fetching user information:", error);
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 exports.createUser = async (req, res) => {
+  logger.info('POST: ENTERING createUser controller method');
+  statsDClient.increment('endpoints.request.http.post.createUser');
   if (Object.keys(req.query).length > 0) {
-    return res
-      .status(400)
-      .json({ message: "Bad Request: Query parameters are not allowed" });
+    logger.warn('POST: createUser called with query parameters');
+    return res.status(400).json({ message: "Query parameters are not allowed" });
   }
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    logger.warn('POST: Validation errors in createUser');
+    statsDClient.increment('endpoints.response.http.post.fail.validationError.createUser');
     return res.status(400).json({ errors: errors.array() });
   }
 
   const { first_name, last_name, email, password } = req.body;
-
   try {
     const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
+      logger.warn('POST: User already exists - createUser');
+      statsDClient.increment('endpoints.response.http.post.fail.userExists.createUser');
       return res.status(400).json({ message: "User already exists" });
     }
 
@@ -52,9 +63,8 @@ exports.createUser = async (req, res) => {
       email,
       password,
     });
-
     res.header("Accept", "application/json");
-
+    statsDClient.increment('endpoints.response.http.post.success.createUser');
     return res.status(201).json({
       id: newUser.id,
       first_name: newUser.first_name,
@@ -64,26 +74,32 @@ exports.createUser = async (req, res) => {
       account_updated: newUser.account_updated,
     });
   } catch (error) {
+    logger.error('POST: Error creating user');
     console.error("Error creating user:", error);
+    statsDClient.increment('endpoints.response.http.post.failure.createUser');
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 exports.updateUser = async (req, res) => {
+  logger.info('PUT: ENTERING updateUser controller method');
+  statsDClient.increment('endpoints.request.http.put.updateUser');
   if (Object.keys(req.query).length > 0 || Object.keys(req.body).length === 0) {
-    return res.status(400).send();
+    logger.warn('PUT: Invalid request in updateUser');
+    statsDClient.increment('endpoints.response.http.put.fail.requestValidation.updateUser');
+    return res.status(400).send({ message: "Invalid request parameters" });
   }
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).send();
+    logger.warn('PUT: Validation errors in updateUser');
+    statsDClient.increment('endpoints.response.http.put.fail.validationError.updateUser');
+    return res.status(400).send({ errors: errors.array() });
   }
 
   const { first_name, last_name, email, password } = req.body;
-
   try {
     const user = req.user;
-
     const updatedUserData = {
       first_name: first_name || user.first_name,
       last_name: last_name || user.last_name,
@@ -97,10 +113,12 @@ exports.updateUser = async (req, res) => {
     }
 
     await User.update(updatedUserData, { where: { id: user.id } });
-
+    statsDClient.increment('endpoints.response.http.put.success.updateUser');
     return res.status(204).send();
   } catch (error) {
+    logger.error('PUT: Error updating user information');
     console.error("Error updating user information:", error);
+    statsDClient.increment('endpoints.response.http.put.failure.updateUser');
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
